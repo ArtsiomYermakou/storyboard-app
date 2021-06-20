5a1247bcf2340f0ec9eda98a890b635cb6d9dbce
ArtsiomYermakou <artem_ermakov_1999@mail.ru>
2021-06-20 20:17:12 +0300

    test screen

--- storyboard-app/ViewController.swift
+++ storyboard-app/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var count: Int = 0;
+    
+    @IBAction func printHello(){
+        if count <= 9{
+            count += 1
+            print("Hello! :) \(count)")
+        } else if count == 10{
+            print("Max value.")
+        }
+    }
+    
+    @IBAction func decrement(){
+        if count != 0 {
+            count -= 1
+            print("decrement. \(count)")
+        }
+        
+    }
 }
-
